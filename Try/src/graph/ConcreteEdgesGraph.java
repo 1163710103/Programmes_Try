fed9cde0d3b50a0181fbd281f2c8c3d67af9cbfc
unknown <>
2018-04-06 22:24:19 +0800

    0406

---
+++ Try/src/graph/ConcreteEdgesGraph.java
@@ -0,0 +1,167 @@
+/* Copyright (c) 2015-2016 MIT 6.005 course staff, all rights reserved.
+ * Redistribution of original or derived work requires permission of course staff.
+ */
+package graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.sun.scenario.effect.impl.prism.PrImage;
+
+/**
+ * An implementation of Graph.
+ * 
+ * <p>PS2 instructions: you MUST use the provided rep.
+ */
+public class ConcreteEdgesGraph implements Graph<String> {
+    
+    private final Set<String> vertices = new HashSet<>();
+    private final List<Edge> edges = new ArrayList<>();
+    
+    // Abstraction function:
+    //   TODO
+    // Representation invariant:
+    //   TODO
+    // Safety from rep exposure:
+    //   TODO
+    
+    // TODO constructor
+    
+    // TODO checkRep
+    
+    @Override public boolean add(String vertex) {
+        if(vertices.contains(vertex)) {
+        	return false;
+        }
+        else {
+        	vertices.add(vertex);
+        	return true;
+        }
+    }
+    
+    @Override public int set(String source, String target, int weight) {
+    	int previous = 0;
+        for(int i = 0;i < edges.size();i++){
+        	if((edges.get(i).getSource() == source)&&(edges.get(i).getTarget() == target)) {
+        		previous = edges.get(i).getWeight();
+        		if(weight == 0) {
+        			edges.remove(i);
+        			return previous;
+        		}
+        		else {
+        			edges.get(i).setWeight(weight);
+        			return previous;
+        		}
+        	}
+        }
+        Edge newedge = new Edge(weight,source,target);
+        edges.add(newedge);
+        return previous;
+     
+    }
+    
+    @Override public boolean remove(String vertex) {
+    	if(vertices.contains(vertex)) {
+    		vertices.remove(vertex);
+    		for(int i = 0;i < edges.size();i++){
+    			if(edges.get(i).getSource() == vertex) {
+    				edges.remove(i);
+    			}
+    			else if(edges.get(i).getTarget() == vertex){
+    				edges.remove(i);
+    			}
+    		}
+        	return true;
+        }
+        else {
+        	return false;
+        }
+    }
+    
+    @Override public Set<String> vertices() {
+        return this.vertices;
+    }
+    
+    @Override public Map<String, Integer> sources(String target) {
+        Map<String ,Integer> source_edge = new HashMap<>();
+        for(int i = 0;i < edges.size();i++) {
+        	if(edges.get(i).getTarget() == target) {
+        		source_edge.put(edges.get(i).getSource(),edges.get(i).getWeight());
+        	}
+        }
+        return source_edge;
+    }
+    
+    @Override public Map<String, Integer> targets(String source) {
+    	Map<String ,Integer> target_edge = new HashMap<>();
+        for(int i = 0;i < edges.size();i++) {
+        	if(edges.get(i).getSource() == source) {
+        		target_edge.put(edges.get(i).getTarget(),edges.get(i).getWeight());
+        	}
+        }
+        return target_edge;    
+        }
+    
+    // TODO toString()
+    
+}
+
+/**
+ * TODO specification
+ * Immutable.
+ * This class is internal to the rep of ConcreteEdgesGraph.
+ * 
+ * <p>PS2 instructions: the specification and implementation of this class is
+ * up to you.
+ */
+class Edge {
+    
+    // TODO fields
+    private String target;
+    private String source;
+    private int weight;
+    // Abstraction function:
+    //   TODO
+    // Representation invariant:
+    //   TODO
+    // Safety from rep exposure:
+    //   TODO
+    
+    // TODO constructor
+    public Edge(int weight,String source,String target) {
+    	this.weight = weight;
+    	this.source = source;
+    	this.target = target;
+    }
+    // TODO checkRep
+    
+    // TODO methods
+    public String getTarget() {
+    	return target;
+    }
+    public String getSource() {
+    	return source;
+    }
+    public int getWeight() {
+    	return weight;
+    }
+    public boolean setTarget(String target) {
+    	this.target = target;
+    	return true;
+    }
+    public boolean setSource(String source) {
+    	this.source = source;
+    	return true;
+    }
+    public boolean setWeight(int weight) {
+    	this.weight = weight;
+    	return true;
+    }
+    	
+    // TODO toString()
+    
+}
---
+++ Try/test/graph/ConcreteEdgesGraphTest.java
@@ -0,0 +1,120 @@
+/* Copyright (c) 2015-2016 MIT 6.005 course staff, all rights reserved.
+ * Redistribution of original or derived work requires permission of course staff.
+ */
+package graph;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+/**
+ * Tests for ConcreteEdgesGraph.
+ * 
+ * This class runs the GraphInstanceTest tests against ConcreteEdgesGraph, as
+ * well as tests for that particular implementation.
+ * 
+ * Tests against the Graph spec should be in GraphInstanceTest.
+ */
+public class ConcreteEdgesGraphTest extends GraphInstanceTest {
+    
+    /*
+     * Provide a ConcreteEdgesGraph for tests in GraphInstanceTest.
+     */
+    @Override public Graph<String> emptyInstance() {
+        return new ConcreteEdgesGraph();
+    }
+    
+
+    /*
+     * Testing ConcreteEdgesGraph...
+     */
+    @Test
+    public void addTest() throws Exception{
+    	Graph<String> graph = emptyInstance();
+    	boolean S1 = graph.add("A");
+    	boolean S2 = graph.add("A");
+    	assertTrue(S1);
+    	assertFalse(S2);	
+    }
+    
+    @Test 
+    public void setTest() throws Exception{
+    	Graph<String> graph = emptyInstance();
+    	graph.add("A");
+    	graph.add("B");
+    	graph.add("C");
+    	assertEquals(0,graph.set("A","B",1));
+    	assertEquals(1,graph.set("A","B",5));
+    	assertEquals(5,graph.set("A","B",0));
+    	assertEquals(0,graph.set("A","B",3));
+    	assertEquals(0,graph.set("A","C",3));
+    }
+    
+    @Test
+    public void removeTest() throws Exception{
+    	Graph<String> graph = emptyInstance();
+    	graph.add("A");
+    	graph.add("B");
+    	graph.add("C");
+    	graph.set("A","B",1);
+    	graph.set("A","C",3);
+    	assertFalse(graph.remove("D"));
+    	assertTrue(graph.remove("A"));
+    	assertTrue(graph.remove("C"));
+    }
+    
+    @Test 
+    public void sourcesTest() throws Exception{
+    	Graph<String> graph = emptyInstance();
+    	graph.add("A");
+    	graph.add("B");
+    	graph.add("C");
+    	graph.set("A","B",1);
+    	graph.set("A","C",3);
+    	Map<String ,Integer> sor0 =new HashMap<>();
+    	Map<String ,Integer> sor1 =new HashMap<>();
+    	Map<String ,Integer> sor2 =new HashMap<>();
+    	sor1.put("A",1);
+    	sor2.put("A",3);
+    	assertEquals(sor0,graph.sources("A"));
+    	assertEquals(sor1,graph.sources("B"));
+    	assertEquals(sor2,graph.sources("C"));
+
+
+    }
+    @Test
+    public void targetsTest()throws Exception{
+    	Graph<String> graph = emptyInstance();
+    	graph.add("A");
+    	graph.add("B");
+    	graph.add("C");
+    	graph.set("A","B",1);
+    	graph.set("A","C",3);
+    	Map<String ,Integer> sor1 =new HashMap<>();
+    	Map<String ,Integer> sor2 =new HashMap<>();
+    	sor1.put("B",1);
+    	sor1.put("C",3);
+    	assertEquals(sor1,graph.targets("A"));
+    	assertEquals(sor2,graph.targets("C"));
+    	
+
+
+    }
+    // Testing strategy for ConcreteEdgesGraph.toString()
+    //   TODO
+    
+    // TODO tests for ConcreteEdgesGraph.toString()
+    
+    /*
+     * Testing Edge...
+     */
+    
+    // Testing strategy for Edge
+    //   TODO
+    
+    // TODO tests for operations of Edge
+    
+}
